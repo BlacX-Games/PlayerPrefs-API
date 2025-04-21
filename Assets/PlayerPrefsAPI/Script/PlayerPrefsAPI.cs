659f8c7640b6ee903bc0846a4c12ae4a6a7fbd6a
Shayan <shayan.officeal@gmail.com>
2025-04-21 14:13:00 -0700

    chore: update namespace

--- Assets/PlayerPrefsAPI/Script/PlayerPrefsAPI.cs
+++ Assets/PlayerPrefsAPI/Script/PlayerPrefsAPI.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-namespace BlacXGames
+namespace BlacXGames.Persistence.PlayerPrefs
 {
     public class PlayerPrefsAPI
     {
@@ -14,19 +13,19 @@ namespace BlacXGames
             private const string dateofbirth = "_dob";
             private const string gender = "_gender";
             
-            public static string FullName { get => PlayerPrefs.GetString(fullname); set => PlayerPrefs.SetString(fullname, value); }
-            public static string FirstName { get => PlayerPrefs.GetString(firstname); set => PlayerPrefs.SetString(firstname, value); }
-            public static string LastName { get => PlayerPrefs.GetString(lastname); set => PlayerPrefs.SetString(lastname, value); }
-            public static string DateOfBirth { get => PlayerPrefs.GetString(dateofbirth); set => PlayerPrefs.SetString(dateofbirth, value); }
-            public static string Gender { get => PlayerPrefs.GetString(gender); set => PlayerPrefs.SetString(gender, value); }
+            public static string FullName { get => UnityEngine.PlayerPrefs.GetString(fullname); set => UnityEngine.PlayerPrefs.SetString(fullname, value); }
+            public static string FirstName { get => UnityEngine.PlayerPrefs.GetString(firstname); set => UnityEngine.PlayerPrefs.SetString(firstname, value); }
+            public static string LastName { get => UnityEngine.PlayerPrefs.GetString(lastname); set => UnityEngine.PlayerPrefs.SetString(lastname, value); }
+            public static string DateOfBirth { get => UnityEngine.PlayerPrefs.GetString(dateofbirth); set => UnityEngine.PlayerPrefs.SetString(dateofbirth, value); }
+            public static string Gender { get => UnityEngine.PlayerPrefs.GetString(gender); set => UnityEngine.PlayerPrefs.SetString(gender, value); }
         }
         public static class Currency
         {
             private const string coin = "_coin";
             private const string gem = "_gem";
             
-            public static string Coin { get => PlayerPrefs.GetString(coin); set => PlayerPrefs.SetString(coin, value); }
-            public static string Gem { get => PlayerPrefs.GetString(gem); set => PlayerPrefs.SetString(gem, value); }
+            public static string Coin { get => UnityEngine.PlayerPrefs.GetString(coin); set => UnityEngine.PlayerPrefs.SetString(coin, value); }
+            public static string Gem { get => UnityEngine.PlayerPrefs.GetString(gem); set => UnityEngine.PlayerPrefs.SetString(gem, value); }
         }
         public static class Setting
         {
@@ -34,9 +33,9 @@ namespace BlacXGames
             private const string sound = "_sound";
             private const string vibration = "_vibration";
             
-            public static bool Music { get => bool.Parse(PlayerPrefs.GetString(music)); set => PlayerPrefs.SetString(music, value.ToString()); }
-            public static bool Sound { get => bool.Parse(PlayerPrefs.GetString(sound)); set => PlayerPrefs.SetString(sound, value.ToString()); }
-            public static bool Vibration { get => bool.Parse(PlayerPrefs.GetString(vibration)); set => PlayerPrefs.SetString(vibration, value.ToString()); }
+            public static bool Music { get => bool.Parse(UnityEngine.PlayerPrefs.GetString(music)); set => UnityEngine.PlayerPrefs.SetString(music, value.ToString()); }
+            public static bool Sound { get => bool.Parse(UnityEngine.PlayerPrefs.GetString(sound)); set => UnityEngine.PlayerPrefs.SetString(sound, value.ToString()); }
+            public static bool Vibration { get => bool.Parse(UnityEngine.PlayerPrefs.GetString(vibration)); set => UnityEngine.PlayerPrefs.SetString(vibration, value.ToString()); }
         }
     }
 }
